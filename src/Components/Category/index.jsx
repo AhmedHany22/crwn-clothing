6a26cf567d6f1894e6d8630414efcf8d5726da92
Ahmed Hany <ahmed_hany19@outlook.com>
2022-06-28 23:56:27 +0200

    Category Component

---
+++ src/Components/Category/index.jsx
@@ -0,0 +1,16 @@
+import "./Category.styles.scss";
+
+const Category = ({ category }) => (
+  <div className="category-container">
+    <div
+      className="background-image"
+      style={{ backgroundImage: `${category.imageUrl}` }}
+    />
+    <div className="category-body-container">
+      <h2>{category.title}</h2>
+      <p>Shop Now</p>
+    </div>
+  </div>
+);
+
+export default Category;
