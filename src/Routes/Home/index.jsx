2c0ca14394af5868ce604d77a6cc6b5d88cc3eb8
Ahmed Hany <ahmed_hany19@outlook.com>
2022-06-29 13:59:51 +0200

    Home Route

---
+++ src/Routes/Home/index.jsx
@@ -0,0 +1,20 @@
+import Directory from "../../Components/Directory";
+import "./Home.styles.scss";
+
+import { useState, useEffect } from "react";
+
+const Home = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data);
+      });
+  }, []);
+
+  return <Directory categories={categories} />;
+};
+
+export default Home;
