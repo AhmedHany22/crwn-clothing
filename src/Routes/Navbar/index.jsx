e4e30a75437af84ce158925253edf83105d89734
Ahmed Hany <ahmed_hany19@outlook.com>
2022-06-29 14:53:45 +0200

    Navbar Route

---
+++ src/Routes/Navbar/index.jsx
@@ -0,0 +1,27 @@
+import "./Navbar.styles.scss";
+import { Outlet, Link } from "react-router-dom";
+import React from "react";
+import { ReactComponent as Crown } from "../../assets/crown.svg";
+
+const Navbar = () => {
+  return (
+    <React.Fragment>
+      {/* Navegation Bar */}
+      <div className="navigation">
+        <Link to="/" className="logo-container">
+          <Crown className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          {/* Routes Links */}
+          <Link to="/shop" className="nav-link">
+            Shop
+          </Link>
+        </div>
+      </div>
+      {/* Pages Container */}
+      <Outlet />
+    </React.Fragment>
+  );
+};
+
+export default Navbar;
