0c4d9a9bd9e4ce8a20a3e98617690dfa0a020a18
Ahmed Hany <ahmed_hany19@outlook.com>
2022-07-11 12:55:26 +0200

    Create the "Firebase.utils" file + Add Google Signin Popup btn

--- src/Routes/Authentication/index.js
+++ src/Routes/Authentication/index.js
@@ -1,7 +1,24 @@
+// In the firebase Database -> Enable Authentication -> Signin-method -> Add Provider -> Google
+import {
+  signInWithGooglePopup,
+  createUserDoc,
+} from "../../Utils/Firebase/firebase.utils";
+import React from "react";
+
 import "./Authentication.styles.scss";
 
 const Authentication = () => {
-  return <h1>Authentication</h1>;
+  // Gitting the Respone of signing-in with Google-Popup
+  const googleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDoc(user);
+  };
+  return (
+    <React.Fragment>
+      <h1>Authentication</h1>
+      <button onClick={googleUser}>Google Sign Popup</button>
+    </React.Fragment>
+  );
 };
 
 export default Authentication;
---
+++ src/Utils/Firebase/firebase.utils.js
@@ -0,0 +1,74 @@
+// imort the firebase collection of tools as "App"
+import { initializeApp } from "firebase/app";
+
+// import firestore methods : Get-> to create firestore instance, Doc-> to retrive docs
+// getDoc & setDoc to read & update the document data
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// import the "Firebase Auth" library
+import {
+  getAuth,
+  signinWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+// Firebase "App" configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCR53Uq3QjMHiinLdkkswB_yIuIOQpCCFw" /*Not a secret */,
+  authDomain: "crown-clothes-d.firebaseapp.com",
+  projectId: "crown-clothes-d",
+  storageBucket: "crown-clothes-d.appspot.com",
+  messagingSenderId: "292727571717",
+  appId: "1:292727571717:web:5e4675d8a75fb6f7e0e36c",
+};
+
+// Initialize the Firebase instanse to control the CRUD operations
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Initialize the provider of Google Auth : 1 of many usages of Google Auth
+// It's a class from firebase auth which can be used to create multi providers
+const provider = new GoogleAuthProvider();
+
+// How to act when somebody interact with provider
+provider.setCustomParameters({ prompt: "select_account" });
+
+// The way the app authenticate with firebase : It's a func because its the same at all times
+export const auth = getAuth();
+
+// Export Signing with Google-Popup function
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+// Initialize firestore DB
+export const db = getFirestore();
+
+// Creating a user Document from "userAuthObj"
+export const createUserDoc = async (userAuth) => {
+  // Creating a Doc ref with 3 prams [The DB, The collection name, A unique id]
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  // Creating a snapshot from the DocRef to be able to test if it exists
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot.exists());
+
+  // If the user Doc doesn't exist then create new one
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  // If the user Doc exists
+  return userDocRef;
+};
