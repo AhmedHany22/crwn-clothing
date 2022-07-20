95975db3aaa4ebca3c4adc2940ac9a6a6aec3bed
Ahmed Hany <ahmed_hany19@outlook.com>
2022-07-20 15:20:20 +0200

    Creating ProductCards & ShopingList

---
+++ src/Components/ProductCard/index.js
@@ -0,0 +1,18 @@
+import "./ProductCard.styles.scss";
+import Button from "./../Button/index";
+
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
+  return (
+    <div className="product-card-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <Button btnType="inverted">Add to Cart</Button>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/Routes/Shop/index.jsx
+++ src/Routes/Shop/index.jsx
@@ -1,13 +1,14 @@
 import "./Shop.styles.scss";
 import { useContext } from "react";
 import { ProductsContext } from "./../../Context/products.context";
+import ProductCard from "./../../Components/ProductCard/index";
 
 const Shop = () => {
   const { products } = useContext(ProductsContext);
   return (
-    <div>
-      {products.map(({ id, name }) => (
-        <div key={id}>{name}</div>
+    <div className="products-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   );
