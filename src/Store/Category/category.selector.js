90d530655ddb3679f380b5dee78c5ed53b0f3619
Ahmed Hany <ahmed_hany19@outlook.com>
2022-08-02 01:04:46 +0200

    Applying memoaization using function { createSelector }

--- src/Store/Category/category.selector.js
+++ src/Store/Category/category.selector.js
@@ -1,8 +1,22 @@
-// Selector to get Redux categories Data + The 1 of the patterns that emerge with redux
-// The selector is where the reducer specific transformation logic is going to live.
-export const categorySelector = (state) =>
-  state.category.categories.reduce((accumlator, category) => {
-    const { title, items } = category;
-    accumlator[title.toLowerCase()] = items;
-    return accumlator;
-  }, {});
+// It's a func that perform memoaization technique
+import { createSelector } from "reselect";
+
+const selectCategoryReducerState = (state) => state.category;
+
+// createSelector first param is the input for the last param
+// There can be multible params, if 5 then 4 are inputs for the last which must be func
+// The last param the func will only run if any of the first params changed
+const selectCategory = createSelector(
+  selectCategoryReducerState,
+  (categorySlice) => categorySlice.categories
+);
+
+export const categorySelector = createSelector(
+  selectCategory,
+  (categorySliceMap) =>
+    categorySliceMap.reduce((accumlator, category) => {
+      const { title, items } = category;
+      accumlator[title.toLowerCase()] = items;
+      return accumlator;
+    }, {})
+);
